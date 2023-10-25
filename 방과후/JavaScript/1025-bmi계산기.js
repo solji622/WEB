b0be5af0412f4c34b82985f5256794133d2fb8dc
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-10-25 17:26:52 +0900

    Update 1025-bmi계산기.js

--- 방과후/JavaScript/1025-bmi계산기.js
+++ 방과후/JavaScript/1025-bmi계산기.js
@@ -4,25 +4,17 @@ var height = prompt("키?") / 100
 
 var bmi = weight / (height * height);
 
+document.write("weight: " + weight, "<br>")
+document.write("height: " + height, "<br>")
+document.write("bmi: " + bmi, "<br>")
+
 if (bmi < 18.5) {
-    document.write("weight: " + weight, "<br>")
-    document.write("height: " + height, "<br>")
-    document.write("bmi: " + bmi, "<br>")
     document.write("저체중", "<br>")
 } else if (bmi < 25) {
-    document.write("weight: " + weight, "<br>")
-    document.write("height: " + height, "<br>")
-    document.write("bmi: " + bmi, "<br>")
     document.write("건강", "<br>")
 } else if (bmi < 30) {
-    document.write("weight: " + weight, "<br>")
-    document.write("height: " + height, "<br>")
-    document.write("bmi: " + bmi, "<br>")
     document.write("과체중", "<br>")
 } else if (bmi >= 30) {
-    document.write("weight: " + weight, "<br>")
-    document.write("height: " + height, "<br>")
-    document.write("bmi: " + bmi, "<br>")
     document.write("비만", "<br>")
 }
 
