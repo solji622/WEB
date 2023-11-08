e096b79b38603167cb3044bb8191da83ae865175
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-11-08 10:31:36 +0900

    Create main.js

---
+++ 과학(과세특)/main.js
@@ -0,0 +1,17 @@
+function setDisplayAppreance(self){
+    var body = document.querySelector('body'); 
+    var btn = document.getElementById('btnDisplayMode'); 
+    if(self.value === '🌗 다크모드로 보기'){  //다크모드로 전환 버튼을 누른 경우
+      body.style.backgroundColor = 'black';  //배경색 변경
+      body.style.color = 'white'; //css가 적용되지 않은 기본 텍스트 색상 변경
+      btn.style.backgroundColor = "white"; //버튼의 배경색 변경
+      btn.style.color = "black"; //버튼의 텍스트 색상 변경
+      self.value = '🌝 라이트모드로 보기';
+    } else { //라이트모드로 전환 버튼을 누른 경우
+      body.style.backgroundColor = 'white';
+      body.style.color = 'black';
+      btn.style.backgroundColor = "black";
+      btn.style.color = "white";
+      self.value = '🌗 다크모드로 보기';
+    }
+}
